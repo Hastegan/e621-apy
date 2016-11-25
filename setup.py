7cd414d021e7abb0d5aa01976af781d5b78cea92
Viljo-Kovanen <175069@gmail.com>
2016-11-25 15:41:45 +0100

    Add setup file

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+      name='e621apy',
+      description='A python layer for the e621 api',
+      url='http://github.com/Hastegan/e621-apy',
+      version='0.1',
+      author='Hastegan',
+      author_email='hastegan@durbatuluk.net',
+      licence='',
+      platforms=['any'],
+      packages=find_packages(),
+      include_package_data=True,
+      download_url='',
+)
